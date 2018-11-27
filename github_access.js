924f10d934b3c45c19ace85264bec01e6d361cdf
Sabah Khan <khans3@tcd.ie>
2018-11-27 12:53:18 +0000

    Fetching repo data from unauthenticated user input from user

--- github_access.js
+++ github_access.js
@@ -1,6 +1,22 @@
-var github = require ('octonode');
+var github = require('octonode');
 var client = github.client();
 
-client.get('/users/KSabah', {}, function (err, status, body, headers) {
-    console.log(body); //json object
-  });
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What user would you like info on? ',(answer) =>
+{
+  var ghuser = client.user(answer);
+
+  ghuser.repos((function(err, data, headers) {
+    console.log("data: " + JSON.stringify(data));
+    /*Uncomment to view errors and headers
+    console.log("error: " + err);
+    console.log("headers:" + headers);
+    */
+  }));
+  rl.close();
+});
