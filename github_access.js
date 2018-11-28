7d582b21b13c1506ac48968f6dddd17520695089
Sabah Khan <khans3@tcd.ie>
2018-11-28 16:32:07 +0000

    Updated code to display the amount of a user's commits to their repositories

--- github_access.js
+++ github_access.js
@@ -1,6 +1,5 @@
 var github = require('octonode');
 var client = github.client();
-
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,17 +9,18 @@ const rl = readline.createInterface({
 rl.question('What user would you like info on? ',(answer) =>
 {
   var ghuser = client.user(answer);
-  /* Uncomment to show rate limit details
-  client.limit(function (err, left, max, reset) {
-  console.log(left);
-  console.log(max);
-  console.log(reset); (UTC epoch seconds)
-  */
   ghuser.repos((function(err, data, headers) {
-    for(var name in data) {
-        if(data.hasOwnProperty(name))
-            console.log(data[name].name);
-    }
-  }));
-  rl.close();
+    for(var i in data) {
+      if(data.hasOwnProperty(i)){
+        var repoName = data[i].name;
+        getCommits(answer, repoName);
+      }
+   }
+ }));
+rl.close();
 });
+
+async function getCommits (answer, repoName) {
+  var res = await client.getAsync('/repos/'+answer+'/'+repoName+'/contributors', {});
+  console.log(repoName+' : '+ res[1][0].contributions)
+}
